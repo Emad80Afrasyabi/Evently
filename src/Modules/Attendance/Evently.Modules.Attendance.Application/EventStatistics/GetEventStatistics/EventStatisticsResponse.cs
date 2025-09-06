14b8aec9b4c0e56166e8ddbfdd58413ace7aac3a
Emad80Afrasyabi <mohammademadafrasyabi@gmail.com>
2025-09-06 19:27:00 +0330

    Add EventStatistics using materializedView

---
+++ src/Modules/Attendance/Evently.Modules.Attendance.Application/EventStatistics/GetEventStatistics/EventStatisticsResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Evently.Modules.Attendance.Application.EventStatistics.GetEventStatistics;
+
+public sealed record EventStatisticsResponse
+{
+    public Guid EventId { get; init; }
+    public string Title { get; init; } = null!;
+    public string Description { get; init; } = null!;
+    public string Location { get; init; } = null!;
+    public DateTime StartsAtUtc { get; init; }
+    public DateTime? EndsAtUtc { get; init; }
+    public int TicketsSold { get; init; }
+    public int AttendeesCheckedIn { get; init; }
+    public string[] DuplicateCheckInTickets { get; init; } = [];
+    public string[] InvalidCheckInTickets { get; init; } = [];
+}
